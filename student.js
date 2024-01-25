51eaf7ec7e820a8e0c3692310ab287779ce638a9
sneha-namaji <snehanamaji0987654321@gmail.com>
2024-01-25 12:22:10 +0530

    iput_feids

--- student.js
+++ student.js
@@ -24,3 +24,29 @@ $(document).ready(function() {
 
     });
 });
+// data input field 30days
+
+
+function setDates() {
+    var today = moment().format('YYYY-MM-DD');
+    var last30Days = moment().subtract(30, 'days').format('YYYY-MM-DD');
+
+    document.getElementById('today').value = today;
+    document.getElementById('last30days').value = last30Days;
+}
+
+function validateDate() {
+    var selectedDate = document.getElementById('customDate').value;
+    var today = moment();
+    var minDate = moment().subtract(60, 'days');
+
+    if (moment(selectedDate).isAfter(today)) {
+        alert('Future dates are not allowed.');
+        document.getElementById('customDate').value = '';
+    } else if (moment(selectedDate).isBefore(minDate)) {
+        alert('Please select a date within the last 60 days.');
+        document.getElementById('customDate').value = '';
+    }
+}
+
+setDates();
